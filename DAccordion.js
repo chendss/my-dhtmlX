a54d65a693da7dc44bbf1fb5eadd18a58a36f1a5
cyl <capslock.ch@outlook.com>
2018-05-07 15:54:22 +0800

    '手风琴的moveTop函数实现'

--- DAccordion.js
+++ DAccordion.js
@@ -3,6 +3,9 @@ class Accordion extends dhtmlXAccordion {
 
     constructor(props) {
         super(props)
+        this.config = {
+            hideSet: new Set([])
+        }
     }
 
     /**
@@ -15,7 +18,7 @@ class Accordion extends dhtmlXAccordion {
      * @param {any} icon 子元素的icon
      * @memberof Accordion
      */
-    item(id, text, isOpen, height, icon) {
+    addItem_(id, text, isOpen, height, icon) {
         return this.addItem(...arguments)
     }
 
@@ -46,6 +49,7 @@ class Accordion extends dhtmlXAccordion {
             this.attachFooter(idOrDom, height)
         }
     }
+
     /**
      * 将页头样式加入组件顶部
      * 
@@ -72,6 +76,7 @@ class Accordion extends dhtmlXAccordion {
         let menuObject = this.accordion.attachMenu(menu)
         return menuObject
     }
+
     /**
      * 添加一个dhtmlxRibbon 在组件顶部
      * 
@@ -131,7 +136,7 @@ class Accordion extends dhtmlXAccordion {
      * @param {any} itemId 子元素id
      * @memberof Accordion
      */
-    clear_icon(itemId) {
+    clearIcon_(itemId) {
         let item = this.chose(itemId)
         if (item !== null) {
             item.clearIcon()
@@ -157,7 +162,277 @@ class Accordion extends dhtmlXAccordion {
      * @param {any} eventId 事件id
      * @memberof Accordion
      */
-    unEvent(eventId) {
+    delEvent(eventId) {
         this.detachEvent(eventId)
+    }
+
+    /**
+     * 去掉页脚
+     * 
+     * @memberof Accordion
+     */
+    delFooter() {
+        this.detachFooter()
+    }
+
+    /**
+     * 去掉页头
+     * 
+     * @memberof Accordion
+     */
+    delHeader() {
+        this.detachHeader()
+    }
+
+    /**
+     * 去掉顶部dhtmlxMenu 
+     * 
+     * @memberof Accordion
+     */
+    delMenu() {
+        this.detachMenu()
+    }
+
+    /**
+     * 去掉顶部dhtmlxRibbon 
+     * 
+     * @memberof Accordion
+     */
+    delRibbon() {
+        this.detachRibbon()
+    }
+
+    /**
+     * 清除掉底部状态栏
+     * 
+     * @memberof Accordion
+     */
+    delStatusBar() {
+        this.detachStatusBar()
+    }
+
+    /**
+     * 清除掉顶部工具栏
+     * 
+     * @memberof Accordion
+     */
+    delToolbar() {
+        this.detachToolbar()
+    }
+
+    /**
+     * 关闭元素可拖拽功能
+     * 默认开启扩展模式
+     * @param {boolean} [isMMode=true] 是否开启扩展模式
+     * @memberof Accordion
+     */
+    openDND(isMMode = true) {
+        this.enableMultiMode()
+        this.enableDND()
+    }
+
+    /**
+     * 开启元素分栏模式
+     * 
+     * @memberof Accordion
+     */
+    openMultiMode() {
+        this.enableMultiMode()
+    }
+
+    /**
+     * 获得所有子元素实例
+     * 
+     * @returns 
+     * @memberof Accordion
+     */
+    items() {
+        let result = []
+        this.forEachItem((cell) => {
+            result.push(cell)
+        })
+        return result
+    }
+
+    /**
+     * 遍历所有子元素
+     * 
+     * @param {any} callBack 
+     * @memberof Accordion
+     */
+    eachItem(callBack) {
+        this.forEachItem(callBack)
+    }
+
+    /**
+     * 返回组件顶部的dhtmlxMenu实例
+     * 
+     * @returns 
+     * @memberof Accordion
+     */
+    getMenu() {
+        let menuObj = this.getAttachedMenu()
+        return menuObj
+    }
+
+    /**
+     * 获得顶部的dhtmlxRibbon 实例
+     * 
+     * @returns 
+     * @memberof Accordion
+     */
+    getRibbon() {
+        let result = this.getAttachedRibbon()
+        return result
+    }
+
+    /**
+     * 获得底部状态栏对象
+     * 
+     * @returns 
+     * @memberof Accordion
+     */
+    getStatusBar() {
+        let result = this.getAttachedStatusBar()
+        return result
+    }
+
+    /**
+     * 获得dhtmlxToolbar 实例
+     * 
+     * @returns 
+     * @memberof Accordion
+     */
+    getToolbar() {
+        let result = this.getAttachedToolbar()
+        return result
+    }
+
+    /**
+     * 获得子元素的文本
+     * 
+     * @param {any} itemId 
+     * @returns 
+     * @memberof Accordion
+     */
+    getItemText(itemId) {
+        let result = this.chose(itemId).getText()
+        return result
+    }
+
+    /**
+     * 获得所有子元素文本
+     * 
+     * @returns 
+     * @memberof Accordion
+     */
+    itemTexts() {
+        let result = []
+        this.forEachItem((cell) => {
+            result.push(cell.getText())
+        })
+        return result
+    }
+
+    /**
+     * 隐藏子元素
+     * 
+     * @param {any} itemId 
+     * @memberof Accordion
+     */
+    hideItem_(itemId) {
+        this.chose(itemId).hide()
+        this.config.hideSet.add(itemId)
+    }
+
+    showItem(itemId) {
+        this.chose(itemId).show()
+        this.config.hideSet.delete(itemId)
+    }
+
+    /**
+     * 子元素隐藏或者显示的开关函数
+     * 
+     * @param {any} itemId 
+     * @memberof Accordion
+     */
+    toggleItem(itemId) {
+        if (this.config.hideSet.has(itemId)) {
+            this.showItem(itemId)
+        } else {
+            this.hide_Item(itemId)
+        }
+    }
+
+    /**
+     * 
+     *隐藏顶部dhtmlxRibbon 对象 
+     * @memberof Accordion
+     */
+    hideRibbon_() {
+        this.hideRibbon()
+    }
+
+    /**
+     * 隐藏底部状态栏
+     * 
+     * @memberof Accordion
+     */
+    hideStatusBar_() {
+        this.hideStatusBar()
+    }
+
+    /**
+     * 隐藏顶部dhtmlxToolbar 对象
+     * 
+     * @memberof Accordion
+     */
+    hideToolbar() {
+        this.hideToolbar()
+    }
+
+    /**
+     * 子元素是否处于展开状态
+     * 
+     * @param {any} itemId 
+     * @returns 
+     * @memberof Accordion
+     */
+    isOpen(itemId) {
+        let result = this.chose(itemId).isOpened()
+        return result
+    }
+
+    /**
+     * 子元素是否隐藏
+     * 
+     * @param {any} itemId 
+     * @returns 
+     * @memberof Accordion
+     */
+    isItemHidden(itemId) {
+        let result = this.chose(itemId).isVisible()
+        return result
+    }
+
+    /**
+     * 读取文档、json、xml等等
+     * 
+     * @param {any} data 
+     * @param {any} callBack 
+     * @memberof Accordion
+     */
+    loadFile(data, callBack) {
+        this.loadStruct(data, callBack)
+    }
+    
+    /**
+     * 子元素移到最上面
+     * 
+     * @param {any} itemId 
+     * @memberof Accordion
+     */
+    moveOnTop(itemId) {
+        this.chose(itemId).moveOnTop()
     }
 }
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', () => {
     var acc = new Accordion('show')
-    acc.item('a1', 'frist', false, null, null)
-    acc.item('a2', 'frist', false, null, null)
-    acc.item('a3', 'frist', false, null, null)
-    document.querySelector('#b1').addEventListener('click',()=>{
-        acc.close_item('a2')
+    acc.addItem_('a1', 'frist', false, null, null)
+    acc.addItem_('a2', 'frist', false, null, null)
+    acc.addItem_('a3', 'frist', false, null, null)
+    document.querySelector('#b1').addEventListener('click', () => {
+        console.log(acc.isOpen('a1'))
     })
 })
